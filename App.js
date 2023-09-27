f8f1c0e57bb70a5f7b5793593bac6b066bf5022c
Asad Mehmood <asadpk80@gmail.com>
2023-09-27 13:08:20 +0500

    Categories Screen inside Navigator | Dependencies @react-navigation/native & react-navigation/native-stack

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import CategoriesScreen from "./screens/CategoriesScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
     <>
-      <StatusBar style="light" />
-      <CategoriesScreen />
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="MealsCategories"
+            component={CategoriesScreen}
+          ></Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
