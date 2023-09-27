30ba05e8c23d1fc4363036acd4d102b22655acfb
Asad Mehmood <asadpk80@gmail.com>
2023-09-27 16:17:28 +0500

    Displaying Meal titles on MealsOverview Screen

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,13 +1,27 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 // import { useRoute } from "@react-navigation/native";
+import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
 
 function MealsOverviewScreen({ route }) {
   // const route = useRoute();
   const categoryID = route.params.categoryID;
 
+  const displayedMeals = MEALS.filter((meal) => {
+    return meal.categoryIds.indexOf(categoryID) >= 0;
+  });
+
+  function renderMealItem(itemData) {
+    return <MealItem title={itemData.item.title} />;
+  }
+
   return (
     <View>
-      <Text>Welcome on Meals Screen {categoryID}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(meal) => meal.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 }
